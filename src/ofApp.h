cb028c6fd6455c074219a0d7b0341fc71270147c
Nick Fox-Gieg <nick@fox-gieg.com>
2019-09-08 02:00:41 +0100

    update to oF 10.1

--- src/ofApp.h
+++ src/ofApp.h
@@ -47,7 +47,7 @@ class ofApp : public ofBaseApp {
 	    int markTime;
 	    bool trigger;
 	    float motionVal;
-	    ofVec2f motionValRaw;
+	    glm::vec2 motionValRaw;
 	    bool isMoving;
 	    int counter;
 	    int counterMax;
